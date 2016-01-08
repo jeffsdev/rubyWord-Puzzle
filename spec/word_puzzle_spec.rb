1b780dbccea94ee7f4ccb122ead13baea86cc06e
Jeff Seymour <seym1585@gmail.com>
2016-01-08 11:48:52 -0800

    add additional spec to cover all cases

--- spec/word_puzzle_spec.rb
+++ spec/word_puzzle_spec.rb
@@ -2,7 +2,10 @@ require('rspec')
 require('word_puzzle')
 
 describe('String#vowels_to_dashes') do
-  it('replaces all vowels in a string with a "-" symbol') do
+  it('replaces all lowercase vowels in a string with a "-" symbol') do
     expect(('Hello World').vowels_to_dashes).to(eq('H-ll- W-rld'))
+  end
+  it('replaces all uppercase vowels in a string with a "-" symbol') do
+    expect(('Ethereal Abyss').vowels_to_dashes).to(eq('-th-r--l -b-ss'))
   end
 end
