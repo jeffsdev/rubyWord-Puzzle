002b49fb9abad843d0a088dde32cba7d44ce2b53
Jeff Seymour <seym1585@gmail.com>
2016-01-08 09:07:57 -0800

    pass first spec to replace vowells in a string with dashes

--- lib/word_puzzle.rb
+++ lib/word_puzzle.rb
@@ -1,10 +1,6 @@
-
 class String
-
   def word_puzzle
-
-
+    string = self.gsub(/[aeiouyAEIOUY]/, "-")
+    return string
   end
-
-
 end
--- spec/word_puzzle_spec.rb
+++ spec/word_puzzle_spec.rb
@@ -1,2 +1,8 @@
 require('rspec')
 require('word_puzzle')
+
+describe('String#word_puzzle') do
+  it('will replace all vowells in a string with a "-" symbol.') do
+    expect(('Hello World').word_puzzle).to(eq('H-ll- W-rld'))
+  end
+end
