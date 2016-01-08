1d9f7347e1b302b8fd575de169527a6e3582ef9c
Jeff Seymour <seym1585@gmail.com>
2016-01-08 10:06:54 -0800

    pass integration test spec

--- app.rb
+++ app.rb
@@ -5,3 +5,8 @@ require('./lib/word_puzzle')
 get('/') do
   erb(:index)
 end
+
+get('/result') do
+  @result = params.fetch('string-input').word_puzzle()
+  erb(:result)
+end
--- spec/word_puzzle_integration_spec.rb
+++ spec/word_puzzle_integration_spec.rb
@@ -1,9 +1,14 @@
 require('capybara/rspec')
 require('./app')
 
-Capybara.app Sinatra::application
+Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe("word puzzle app", {:type => feature}) do
-
+describe('word puzzle app', {:type => :feature}) do
+  it('takes the word or sentence the user inputs and replaces the vowels with a "-" symbol.') do
+    visit('/')
+    fill_in('string-input', :with => 'Hello World')
+    click_button('Submit')
+    expect(page).to have_content('H-ll- W-rld')
+  end
 end
